7aa3f4e0d75c9a55d028d5420a7dc1afca6525bf
supetite <lecamthanhha@gmail.com>
2026-03-17 16:54:16 +0100

    fix(config): remove sitemap and reset base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://supetite.github.io',
-  base: '/', 
+  base: '/lecamthanhha.github.io/', 
   integrations: [mdx(), tailwind()]
 });
