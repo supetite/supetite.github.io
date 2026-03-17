46d2dda05ae1fd68d39abb3ee18c847b6d27cefa
supetite <lecamthanhha@gmail.com>
2026-03-17 16:48:14 +0100

    fix(config): remove sitemap and reset base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-// import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://lecamthanhha.github.io',
-  base: '/',
+  base: '/', 
   integrations: [mdx(), tailwind()]
 });
